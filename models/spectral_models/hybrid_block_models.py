cf442e286e9c9bbb52c0228e2b383844f1737167
Michele Alberti <michele.alberti@unifr.ch>
2019-01-17 01:03:08 +0100

    dirty test for a bug, forgive me

--- models/spectral_models/hybrid_block_models.py
+++ models/spectral_models/hybrid_block_models.py
@@ -483,3 +483,115 @@ class HybridFirstFourier_32x32_Unfixed(HybridFirstFourier_32x32_Fixed):
                  **kwargs):
 
         super().__init__(output_channels=output_channels, input_channels=input_channels, fixed=fixed, **kwargs)
+
+class BaseModel_32x32(nn.Module):
+    """Abstract Model for input with size 32x32"""
+
+    def __init__(self,
+                 first_block,
+                 second_block,
+                 third_block,
+                 output_channels=10,
+                 input_channels=3,
+                 fixed=None,
+                 **kwargs):
+
+        super().__init__()
+
+        self.expected_input_size = (32, 32)
+
+        self.network = nn.Sequential(
+            first_block(28, 28, input_channels, 16, 5, kernel_size_pooling=2, groups_conv=1, fixed=fixed),
+            nn.LeakyReLU(),
+            second_block(12, 12, 16, 32, 3, kernel_size_pooling=2, fixed=fixed),
+            nn.LeakyReLU(),
+            third_block(4, 4, 32, 32, 3, kernel_size_pooling=1, fixed=fixed),
+            nn.LeakyReLU(),
+            Flatten(),
+            nn.Linear(512, output_channels),
+        )
+
+    def forward(self, x):
+        return self.network(x)
+
+@Model
+class Test(BaseModel_32x32):
+    """
+    Network performing cosine transforms.
+    32x32 -> DCTII -> iDCTII -> DCTII
+    """
+
+    def __init__(self, output_channels=10, input_channels=3, **kwargs):
+        super().__init__(
+            MyDiscreteCosine2dConvBlockHybrid,
+            MyInverseDiscreteCosine2dConvBlockHybrid,
+            MyDiscreteCosine2dConvBlockHybrid,
+            fixed=False,
+            output_channels=output_channels,
+            input_channels=input_channels,
+            **kwargs)
+
+    def forward(self, x):
+        return self.network(x)
+
+class MyDiscreteCosine2dConvBlockHybrid(nn.Module):
+    """
+    Defines a Discrete Cosine 2D Hybrid Block. It performs Conv -> Cos2D -> DepthConcat -> 1x1 operations on the input.
+    """
+
+    def __init__(self,
+                 width,
+                 height,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride=1,
+                 fixed=False,
+                 kernel_size_pooling=None,
+                 groups_conv=2):
+
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, groups=groups_conv)
+        self.cos = DctII2d(width, height, fixed=fixed)
+        self._1x1 = nn.Conv2d(2 * out_channels, out_channels, 1, groups=1)
+        self.pool = nn.MaxPool2d(kernel_size=kernel_size_pooling if kernel_size_pooling is not None else kernel_size)
+
+    def forward(self, x):
+        x = self.conv(x)
+        out = self.cos(x)
+        out = torch.cat((out, x), 1)
+        out = self.pool(out)
+        out = self._1x1(out)
+        return out
+
+
+class MyInverseDiscreteCosine2dConvBlockHybrid(nn.Module):
+    """
+    Defines an inverse Discrete Cosine 2D Hybrid Block. It performs Conv -> iCos2D -> DepthConcat -> 1x1 operations on
+    the input.
+    """
+
+    def __init__(self,
+                 width,
+                 height,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride=1,
+                 fixed=False,
+                 kernel_size_pooling=None,
+                 groups_conv=2):
+
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, groups=groups_conv)
+        self.cos = iDctII2d(width, height, fixed=fixed)
+        self._1x1 = nn.Conv2d(2 * out_channels, out_channels, 1, groups=2)
+        self.pool = nn.MaxPool2d(kernel_size=kernel_size_pooling if kernel_size_pooling is not None else kernel_size)
+
+    def forward(self, x):
+        x = self.conv(x)
+        out = self.cos(x)
+        out = torch.cat((out, x), 1)
+        out = self.pool(out)
+        out = self._1x1(out)
+        return out
