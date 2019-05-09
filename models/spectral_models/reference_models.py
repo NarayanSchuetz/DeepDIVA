3a86f421b3c11dc19911f60a63896e0ee3362661
Renthal <albertimichele91@hotmail.com>
2019-05-09 18:07:47 +0200

    updated the baseline model

--- models/spectral_models/reference_models.py
+++ models/spectral_models/reference_models.py
@@ -1,72 +1,35 @@
-"""
- Created by Narayan Schuetz at 07/12/2018
- University of Bern
-
- This file is subject to the terms and conditions defined in
- file 'LICENSE.txt', which is part of this source code package.
-"""
-
-
-from .parts import *
+from .spectral_modules import *
 import torch.nn as nn
 from models.registry import Model
 
 
 @Model
-class PureConv_150x150(nn.Module):
+class BaselineConv(nn.Module):
 
-    def __init__(self,
-                 output_channels=10,
-                 input_channels=3,
-                 fixed=True,
-                 ocl1=32,  # output channels layer 1
+    def __init__(self, output_channels, in_channels=3, ocl1=32,  # output channels layer 1
                  **kwargs):
-
         super().__init__()
 
-        self.expected_input_size = (127, 127)
+        self.expected_input_size = (149, 149)
+        self.features = []
 
-        self.network = nn.Sequential(
-            ConvBlock(None, None, input_channels, ocl1, 7, fixed=fixed, padding=3, stride=2),
+        self.encoder = nn.Sequential(
+            nn.Conv2d(in_channels, ocl1, kernel_size=8, stride=3, padding=0),
             nn.LeakyReLU(),
-            ConvBlock(None, None, ocl1, ocl1*2, 5, fixed=fixed, padding=2, stride=2),
+            nn.Conv2d(ocl1, ocl1 * 2, kernel_size=5, stride=3, padding=1),
             nn.LeakyReLU(),
-            ConvBlock(None, None, ocl1*2, ocl1*4, 3, fixed=fixed, padding=1, stride=1),
+            nn.Conv2d(ocl1 * 2, ocl1 * 4, kernel_size=3, stride=1, padding=1),
             nn.LeakyReLU(),
-            nn.AvgPool2d(kernel_size=32, stride=1),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.AvgPool2d(kernel_size=16, stride=1),
             Flatten(),
-            nn.Linear(ocl1*4, output_channels)
+            nn.Linear(ocl1 * 4, output_channels)
         )
 
     def forward(self, x):
-        return self.network(x)
+        self.features = self.encoder(x)
+        x = self.classifier(self.features)
+        return x
 
-
-@Model
-class PureConv_32x32(nn.Module):
-
-    def __init__(self,
-                 output_channels=10,
-                 input_channels=3,
-                 fixed=True,
-                 ocl1=16, # output channels layer 1
-                 **kwargs):
-
-        super().__init__()
-
-        self.expected_input_size = (32, 32)
-
-        self.network = nn.Sequential(
-            ConvBlock(32, 32, input_channels, ocl1, 3, fixed=fixed, padding=1, stride=1),
-            nn.LeakyReLU(),
-            ConvBlock(32, 32, ocl1, ocl1*2, 3, fixed=fixed, padding=1, stride=1),
-            nn.LeakyReLU(),
-            ConvBlock(32, 32, ocl1*2, ocl1*4, 3, fixed=fixed, padding=1, stride=1),
-            nn.LeakyReLU(),
-            nn.AvgPool2d(kernel_size=32, stride=1),
-            Flatten(),
-            nn.Linear(ocl1*4, output_channels)
-        )
-
-    def forward(self, x):
-        return self.network(x)
