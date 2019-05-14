722d78aece529a13d98230558ac6dfe04ad232c1
Michele Alberti <michele.alberti@unifr.ch>
2019-05-14 16:01:53 +0200

    implemented a deeper baseline

--- models/spectral_models/reference_models.py
+++ models/spectral_models/reference_models.py
@@ -47,6 +47,38 @@ class BaselineConv(nn.Module):
 
 
 @Model
+class BaselineDeep(nn.Module):
+
+    def __init__(self, output_channels=10, in_channels=3, ocl1=32,  # output channels layer 1
+                 **kwargs):
+        super(BaselineConv, self).__init__()
+
+        self.expected_input_size = (149, 149)
+        self.features = []
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(in_channels, ocl1, kernel_size=8, stride=3, padding=0),
+            nn.LeakyReLU(),
+            nn.Conv2d(ocl1, ocl1, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(),
+            nn.Conv2d(ocl1, ocl1 * 2, kernel_size=5, stride=3, padding=1),
+            nn.LeakyReLU(),
+            nn.Conv2d(ocl1 * 2, ocl1 * 4, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.AvgPool2d(kernel_size=16, stride=1),
+            Flatten(),
+            nn.Linear(ocl1 * 4, output_channels)
+        )
+
+    def forward(self, x):
+        self.features = self.encoder(x)
+        return self.classifier(self.features)
+
+
+@Model
 class BaselineRND(nn.Module):
     def __init__(self, output_channels=10, in_channels=3, ocl1=32,  # output channels layer 1
                  fixed=False, **kwargs):
