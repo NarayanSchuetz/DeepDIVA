f4f21e8cf2878434a4a9609f6aa79774932d4509
Michele Alberti <michele.alberti@unifr.ch>
2019-05-17 01:12:09 +0200

    increased functionality

--- sigoptManager.py
+++ sigoptManager.py
@@ -23,8 +23,36 @@ def print_with_pattern(experiment_list, parts):
             if value.data:
                 dict[n.name] = [value.data[0].value, n.progress.observation_count]
     for i in sorted(dict):
-        print('{:100}'.format(i), "\t", dict[i])
+        print('{:75}'.format(i), "\t", dict[i])
     print("Found: " + str(len(dict)) + "\n\n")
+
+def print_best_assignement_with_pattern(experiment_list, parts):
+    for experiment in experiment_list:
+        if all(p in experiment.name for p in parts):
+            value = conn.experiments(experiment.id).best_assignments().fetch()
+            if value.data:
+                best_assignments = conn.experiments(experiment.id).best_assignments().fetch()
+                lr = best_assignments.data[0].assignments['lr']
+                weight_decay = best_assignments.data[0].assignments['weight_decay']
+                # print('{:60}'.format(experiment.name), "\t",
+                #       value.data[0].value, "\t",
+                #       experiment.progress.observation_count, "\t",
+                #       lr, "\t",
+                #       weight_decay)
+                print("cd {}".format(experiment.name.split("/")[0]), "\n",
+                      "cd `ls`\n",
+                      "cd `ls`\n",
+                      "cd {}".format(experiment.name.split("/")[-1]), "\n",
+                      "cd `ls`\n",
+                      "cd `ls`\n",
+                      "cd `ls`\n",
+                      "cd `ls`\n",
+                      "cd lr={}".format(lr), "\n",
+                      "cd `ls`\n",
+                      "cd `ls`\n",
+                      "cd `ls`\n",
+                      "cp -r `ls` /local/scratch/albertim/best/{}{}".format(experiment.name.split("/")[0],experiment.name.split("/")[-1]),  "\n",
+                      "cd /local/scratch/albertim/output")
 
 
 if __name__ == '__main__':
@@ -37,27 +65,21 @@ if __name__ == '__main__':
     for experiment in conn.experiments().fetch().iterate_pages():
         experiment_list.append(experiment)
 
-    #delete_with_pattern(experiment_list, "spectral")
-    print_with_pattern(experiment_list, ["Colorectal", "v2"])
-    print_with_pattern(experiment_list, ["CSG18", "v2"])
-    print_with_pattern(experiment_list, ["CSG863", "v2"])
-    print_with_pattern(experiment_list, ["CB55", "v2"])
+    #delete_with_pattern(experiment_list, "v3")
+    # print_with_pattern(experiment_list, ["Colorectal", "v3"])
+    # print_with_pattern(experiment_list, ["CSG18", "v3"])
+    # print_with_pattern(experiment_list, ["CSG863", "v3"])
+    # print_with_pattern(experiment_list, ["CB55", "v3"])
 
-    print_with_pattern(experiment_list, ["Colorectal", "v3"])
+    print("--------------------------------------------")
+
+    # print_best_assignement_with_pattern(experiment_list, ["Colorectal", "v2"])
+    # print_best_assignement_with_pattern(experiment_list, ["CSG18", "v2"])
+    # print_best_assignement_with_pattern(experiment_list, ["CSG863", "v2"])
+    # print_best_assignement_with_pattern(experiment_list, ["CB55", "v2"])
+
+    print("--------------------------------------------")
+
+    print_with_pattern(experiment_list, ["v6"])
 
     print("Done!")
-
-    #
-    # EXPERIMENT_ID = retrieve_id_by_name(conn,"n__PureConv_32x32_/dataset/CIFAR10")
-    #
-    # if len(EXPERIMENT_ID) > 1:
-    #     print("Experiments have duplicate names! Archive older ones before proceeding.")
-    #     sys.exit(-1)
-    # if not EXPERIMENT_ID:
-    #     print("Experiments not found")
-    #     sys.exit(-1)
-    #
-    # EXPERIMENT_ID = EXPERIMENT_ID[0]
-    # best_assignments = conn.experiments(EXPERIMENT_ID).best_assignments().fetch()
-    # lr = best_assignments.data[0].assignments['lr']
-    # momentum = best_assignments.data[0].assignments['momentum']
